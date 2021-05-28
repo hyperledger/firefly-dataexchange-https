0a567441a34b7045b4934c11746ed556d6e251d2
Gabriel Indik <gabriel.indik@kaleido.io>
2021-05-28 10:43:16 -0400

    Add copyright headers
    Signed-off-by: Gabriel Indik <gabriel.indik@kaleido.io>

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+// Copyright © 2021 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import express from 'express';
 import https from 'https';
 import http from 'http';
@@ -58,8 +74,6 @@ export const start = async () => {
     }
   });
   
-  // eventsHandler.eventEmitter.addListener('event', event => wss.clients.forEach(client => client.send(JSON.stringify(event))));
-
   const assignWebSocketDelegate = (webSocket: WebSocket) => {
     delegatedWebSocket = webSocket;
     const event = eventsHandler.getCurrentEvent();
