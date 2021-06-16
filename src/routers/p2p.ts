063f8e93ba1e7c4cf015caf79d2aae90bd743db6
Gabriel Indik <gabriel.indik@kaleido.io>
2021-06-16 16:13:18 -0400

    Add metadata
    Signed-off-by: Gabriel Indik <gabriel.indik@kaleido.io>
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- src/routers/p2p.ts
+++ src/routers/p2p.ts
@@ -50,13 +50,14 @@ router.put('/blobs/*', async (req, res, next) => {
     const sender = cert.issuer.O + cert.issuer.OU;
     const file = await utils.extractFileFromMultipartForm(req);
     const blobPath = path.join(utils.constants.RECEIVED_BLOBS_SUBDIRECTORY, sender, req.params[0]);
-    const hash = await blobsHandler.storeBlob(file, blobPath);
+    const metadata = await blobsHandler.storeBlob(file, blobPath);
     res.sendStatus(204);
     eventEmitter.emit('event', {
       type: 'blob-received',
       sender,
       path: blobPath,
-      hash
+      hash: metadata.hash,
+      lastUpdate: metadata.lastUpdate
     } as IBlobReceivedEvent);
   } catch (err) {
     next(err);
